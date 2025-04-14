c5185fa437e18f765faa97c770814dd34454dbbf
Anatoly <56615869+AnatolyLysov@users.noreply.github.com>
2025-04-14 10:58:58 +0000

    Changes to be committed: 	new file:   myfolder/Chapter7/chapter7ex1 	new file:   myfolder/Chapter7/chapter7ex1.c 	renamed:    myfolder/Chapter7/Chapter7ex1.h -> myfolder/Chapter7/chapter7ex1.h
     Changes not staged for commit:
    	modified:   Poker-game (modified content)
    	modified:   pockerWithExchange (modified content)

---
+++ myfolder/Chapter7/chapter7ex1.c
@@ -0,0 +1,32 @@
+// Программа-пример №1 из главы 7 Руководства
+// по С для новичков, 3-е издание
+// Файл Chapter7ex1.c
+/* Это простая программа-пример, перечисляющая трех
+детей, их потребности в школьных принадлежностях,
+а также стоимость их приобретения */
+
+#include <stdio.h>
+#include <string.h>
+#include "chapter7ex1.h"
+
+int main() {
+    int age;
+    char childname[14] = "Тимофей";
+
+    printf("\n%s имеет %d детей", FAMILY, KIDS);
+
+    age = 11;
+    printf("Старший сын, %s, %d лет.\n", childname, age);
+
+    strcpy(childname, "Н");
+//    strcpy(childname, "Николай");
+
+    age = 6;
+    printf("Средний сын, %s, %d.\n", childname, age);
+
+    age = 3;
+    strcpy(childname, "Борис");
+    printf("Младший сын, %s, %d.\n", childname, age);
+
+    return 0;
+}
